ebefd75f80207f8072953724b1a465ae690d5add
redshadowfx <mihai.isarescu@protonmail.com>
2024-01-02 03:30:21 +0200

    added problem1

--- Day3/problem1.go
+++ Day3/problem1.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello day3")
+	inputFile, err := os.Open("day3.input.demo1")
+	defer inputFile.Close()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return 
+	}
+
+	inputScanner := bufio.NewScanner(inputFile)
+	inputScanner.Split(bufio.ScanLines)
+	var engineMap []string
+
+	for inputScanner.Scan(){
+		line := inputScanner.Text()
+		engineMap = append(engineMap, line)
+	}
+
+	for i := 0; i < len(engineMap); i++ {
+		for j := 0; j < len(engineMap[i]); j++ {
+			fmt.Printf("%c ", engineMap[i][j])
+		}
+		fmt.Printf("\n")
+	}
+
+	dotStr := "65$"
+	dot, _ := strconv.Atoi(dotStr)
+	fmt.Printf("\n\nThis is %s as int: %d", dotStr, dot)
 }
