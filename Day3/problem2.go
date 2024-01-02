0c3c1687ab1e44b3c1b60dab2ad0d3e5bb6697b0
redshadowfx <mihai.isarescu@protonmail.com>
2024-01-02 04:59:38 +0200

    Finish problem2

--- Day3/problem2.go
+++ Day3/problem2.go
@@ -51,54 +51,86 @@ func main() {
 
 	for i := 0; i < len(engineMap); i++ {
 		for j := 0; j < len(engineMap[i]); j++ {
-			// find the special characters
-			if !(engineMap[i][j] == '.' || isNumber(engineMap[i][j])) {
-				fmt.Printf("found %c at (%d,%d): \t", engineMap[i][j], i+1, j+1)
+			gearRatio := 1
+			adjacentCount := 0
+			adjacentList := ""
+
+			// find the gear characters
+			if engineMap[i][j] == '*' {
 
 				// find the near numbers
 				if i-1 >= 0 && isNumber(engineMap[i-1][j]) {
 					num := getNumber(engineMap[i-1], j)
-					fmt.Printf("U number(%d) ", num)
-					sum += num
+					adjacentCount++
+					if adjacentCount <= 2 {
+						adjacentList = fmt.Sprintf("%sU number(%d) ", adjacentList, num)
+						gearRatio *= num
+					}
 				} else {
 					if i-1 >= 0 && isNumber(engineMap[i-1][j-1]) {
 						num := getNumber(engineMap[i-1], j-1)
-						fmt.Printf("UL number(%d) ", num)
-						sum += num
+						adjacentCount++
+						if adjacentCount <= 2 {
+							adjacentList = fmt.Sprintf("%sUL number(%d) ", adjacentList, num)
+							gearRatio *= num
+						}
 					}
 					if i-1 >= 0 && isNumber(engineMap[i-1][j+1]) {
 						num := getNumber(engineMap[i-1], j+1)
-						fmt.Printf("UR number(%d) ", num)
-						sum += num
+						adjacentCount++
+						if adjacentCount <= 2 {
+							adjacentList = fmt.Sprintf("%sUR number(%d) ", adjacentList, num)
+							gearRatio *= num
+						}
 					}
 				}
 				if i+1 < len(engineMap) && isNumber(engineMap[i+1][j]) {
 					num := getNumber(engineMap[i+1], j)
-					fmt.Printf("D number(%d) ", num)
-					sum += num
+					adjacentCount++
+					if adjacentCount <= 2 {
+						adjacentList = fmt.Sprintf("%sD number(%d) ", adjacentList, num)
+						gearRatio *= num
+					}
 				} else {
 					if i+1 >= 0 && isNumber(engineMap[i+1][j-1]) {
 						num := getNumber(engineMap[i+1], j-1)
-						fmt.Printf("DL number(%d) ", num)
-						sum += num
+						adjacentCount++
+						if adjacentCount <= 2 {
+							adjacentList = fmt.Sprintf("%sDL number(%d) ", adjacentList, num)
+							gearRatio *= num
+						}
 					}
 					if i+1 >= 0 && isNumber(engineMap[i+1][j+1]) {
 						num := getNumber(engineMap[i+1], j+1)
-						fmt.Printf("DR number(%d) ", num)
-						sum += num
+						adjacentCount++
+						if adjacentCount <= 2 {
+							adjacentList = fmt.Sprintf("%sDR number(%d) ", adjacentList, num)
+							gearRatio *= num
+						}
 					}
 				}
 				if j-1 >= 0 && isNumber(engineMap[i][j-1]) {
 					num := getNumber(engineMap[i], j-1)
-					fmt.Printf("L number(%d) ", num)
-					sum += num
+					adjacentCount++
+					if adjacentCount <= 2 {
+						adjacentList = fmt.Sprintf("%sL number(%d) ", adjacentList, num)
+						gearRatio *= num
+					}
 				}
 				if j+1 < len(engineMap) && isNumber(engineMap[i][j+1]) {
 					num := getNumber(engineMap[i], j+1)
-					fmt.Printf("R number(%d) ", num)
-					sum += num
+					adjacentCount++
+					if adjacentCount <= 2 {
+						adjacentList = fmt.Sprintf("%sR number(%d) ", adjacentList, num)
+						gearRatio *= num
+					}
 				}
-				fmt.Print("\n")
+
+				if adjacentCount == 2 {
+					fmt.Printf("gear found at (%d,%d): %s\t", i+1, j+1, adjacentList)
+					fmt.Print("\n")
+					sum += gearRatio
+				}
 			}
 		}
 	}
