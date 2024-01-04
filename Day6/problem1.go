201b462bc2a4119a1c20ec7d1a40352b13de635d
misarescu <misarescu@deviqon.com>
2024-01-04 14:11:57 +0200

    Solve day 6 part 1

---
+++ Day6/problem1.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	inputFile, err := os.Open("day6.input.official")
+	defer inputFile.Close()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	inputScanner := bufio.NewScanner(inputFile)
+	inputScanner.Split(bufio.ScanLines)
+
+	var times []int
+	var distances []int
+
+	inputScanner.Scan()
+	timeStrLine := inputScanner.Text()
+
+	for _, timeStr := range strings.Split(strings.Split(timeStrLine, ":")[1], " ") {
+		if timeInt, err := strconv.Atoi(timeStr); err == nil {
+			times = append(times, timeInt)
+		}
+	}
+
+	inputScanner.Scan()
+	distanceStr := inputScanner.Text()
+
+	for _, distStr := range strings.Split(strings.Split(distanceStr, ":")[1], " ") {
+		if distInt, err := strconv.Atoi(distStr); err == nil {
+			distances = append(distances, distInt)
+		}
+	}
+
+	totalMargin := 1
+
+	for race := 0; race < len(times); race++ {
+		var hold int
+		for hold = 1; hold <= times[race]/2; hold++ {
+			// found the first hold time to win the race
+			if hold*(times[race]-hold) > distances[race] {
+				break
+			}
+		}
+		margin := times[race] + 1 - 2*hold
+		fmt.Printf("race: %d, first hold: %d, margin: %d\n", race, hold, margin)
+		totalMargin *= margin
+	}
+
+	fmt.Printf("total margin: %d\n", totalMargin)
+	// fmt.Printf("times: %v\n", times)
+	// fmt.Printf("distances: %v\n", distances)
+
+}
